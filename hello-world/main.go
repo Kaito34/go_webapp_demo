9b763d522fc145e5da33762a9113db202236d3be
Kaito34 <zihhkumi@icloud.com>
2023-01-31 18:28:23 +0900

    [add] 3-27

--- hello-world/main.go
+++ hello-world/main.go
@@ -3,16 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+const portNumber = ":8080"
 
-	_ = http.ListenAndServe(":8080", nil)
+// Home is the home page hander
+func Home(w http.ResponseWriter, r *http.Request){
+	renderTemplate(w, "home.page.tmpl")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request){
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
+}
+
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
