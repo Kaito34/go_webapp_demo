df9f4c5f54324f891f1497d70be025c8e3a7bc6e
Kaito34 <zihhkumi@icloud.com>
2023-02-02 17:49:03 +0900

    [add] 5

--- hello-world/pkg/render/render.go
+++ hello-world/pkg/render/render.go
@@ -6,26 +6,48 @@ import (
 	"net/http"
 	"path/filepath"
 	"text/template"
+
+	"github.com/Kaito34/go_webapp_demo/pkg/config"
+	"github.com/Kaito34/go_webapp_demo/pkg/models"
 )
 
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+
+	return td
+}
+
 // RenderTemplate renders templates using http/template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	//create a template cache
-	tc, err := createTemplateCache()
-	if err != nil {
-		log.Fatal(err)
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+	if app.UseCache {
+		// create a template cache
+		// get the template cache from
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
 	}
 
 	// get requested template from cache
 	t, ok := tc[tmpl]
 	if !ok {
-		log.Fatal(err)
+		log.Fatal("could not get template from template cache")
 	}
 
 	// ???
 	buf := new(bytes.Buffer)
 
-	err = t.Execute(buf, nil)
+	td = AddDefaultData(td)
+
+	err := t.Execute(buf, td)
 	if err != nil {
 		log.Println(err)
 	}
@@ -38,7 +60,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 }
 
-func createTemplateCache() (map[string]*template.Template, error) {
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	// myCache := make(map[string]*template.Template)
 	myCache := map[string]*template.Template{}
 
